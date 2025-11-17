39be076be40c64ea6b59c6c7ae82a2f0c9b96add
Aryan Kumar <aryanak9163@gmail.com>
2025-11-18 01:32:35 +0530

    adds invoice controller

---
+++ apps/invoice-service/src/invoice/invoice.controller.ts
@@ -0,0 +1,32 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { InvoiceService } from './invoice.service';
+import { INVOICE_SERVICE_NAME } from '@app/proto/src';
+
+@Controller()
+export class InvoiceController {
+    constructor(private readonly invoiceService: InvoiceService) {}
+
+    @GrpcMethod(INVOICE_SERVICE_NAME, 'FindOne')
+    findOne(data: { id: string }) {
+        return this.invoiceService.findOne(data);
+    }
+
+    @GrpcMethod(INVOICE_SERVICE_NAME, 'FindAll')
+    findAll(data: { limit?: number; offset?: number }) {
+        return this.invoiceService.findAll(data);
+    }
+
+    @GrpcMethod(INVOICE_SERVICE_NAME, 'FindAllByUser')
+    findAllByUser(data: {
+        userId: string;
+        pagination: { limit?: number; offset?: number };
+    }) {
+        return this.invoiceService.findAllByUser(data);
+    }
+
+    @GrpcMethod(INVOICE_SERVICE_NAME, 'Create')
+    create(data: { userId: string; description: string; amount: number }) {
+        return this.invoiceService.create(data);
+    }
+}
