e0672a91b0fd60b55f0f5ef372d8fd13706f4f71
Aryan Kumar <aryanak9163@gmail.com>
2025-11-18 01:36:31 +0530

    adds gateway entry point

---
+++ apps/gateway/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    const logger = new Logger('Gateway');
+
+    const port = configService.get<number>('PORT', 3000);
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+    await app.listen(port);
+    logger.log(`Gateway is listening on port ${port}`);
+}
+bootstrap();
