68dfc3113b02c60312cdd8c3c02db844adb6ccd0
Aryan Kumar <aryanak9163@gmail.com>
2025-11-18 00:24:09 +0530

    adds app module

---
+++ apps/user-service/src/app.module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConsulModule } from '@app/common/src';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
+import * as Joi from 'joi';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string()
+                    .valid('development', 'production')
+                    .default('development'),
+                CONSUL_HOST: Joi.string().required(),
+                SERVICE_ADDRESS: Joi.string().required(),
+                SERVICE_PORT: Joi.number().default(50051),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_DATABASE: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_DATABASE'),
+                entities: [User],
+                synchronize: true, // OK for dev, use migrations in prod
+            }),
+        }),
+        ConsulModule,
+        UserModule,
+    ],
+})
+export class AppModule {}
