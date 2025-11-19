a4eb05c4f6c5441ba69e883c506da547aa8db3d6
Aryan Kumar <aryanak9163@gmail.com>
2025-11-20 02:46:59 +0530

    cors setup for frontend

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,18 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConfigService} from '@nestjs/config';
+import {Logger, ValidationPipe} from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     const logger = new Logger('Gateway');
 
-    const port = configService.get<number>('PORT', 3000);
+    const port = configService.get<number>('PORT', 5000);
 
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
+
+    app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
 
     await app.listen(port);
     logger.log(`Gateway is listening on port ${port}`);
 }
+
 bootstrap();
